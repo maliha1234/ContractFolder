04ed2c2eea26d98ab1c046cb2f1ebb2f0921691a
Maliha Sarwat <malihasarwat@Malihas-MacBook-Pro.local>
2020-08-20 23:28:17 -0500

    chi test demo run

--- chitest.py
+++ chitest.py
@@ -9,14 +9,17 @@ from bioinfokit.analys import stat, get_data
 #dataset = pd.read_csv("/Users/malihasarwat/Documents/Summer2020/Sir/FengCodes/Demodata/max_min_all_features_0_2.csv")
 #print(dataset.shape)
 #print(dataset.head())
-df = pd.read_csv("/Users/malihasarwat/Documents/Summer2020/Sir/FengCodes/Demodata/max_min_all_features_0_2.csv")
-#df = pd.read_csv("/Users/malihasarwat/Documents/Summer2020/Sir/FeatureList/all_features.csv")
-#print(df.head())
+#df2 = pd.read_csv("/Users/malihasarwat/Documents/Summer2020/Sir/FengCodes/Demodata/max_min_all_features_0_2.csv")
+#df = pd.read_csv("/Users/malihasarwat/Documents/Summer2020/Sir/FengCodes/Demodata//small_all_features.csv")
+df = pd.read_csv("/Users/malihasarwat/Documents/Summer2020/Sir/FengCodes/Demodata/name.csv")
+#df = pd.read_csv("
+#df = df2.head(200)
+
 df = df.set_index('label')
 df = df.drop('name', axis=1)
-#df = df.head(200)
+
 print(df.head())
 res = stat()
 res.chisq(df=df)
 print(res.summary)
-print(res.expected_df)
+#print(res.expected_df)
